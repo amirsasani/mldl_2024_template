57de720e8a300dc271aff32ab192f52b5c49d0be
Amir Sasani <sasani.amir.1998@gmail.com>
2024-06-03 13:23:11 +0200

    Task 3: Actor Critic

---
+++ algorithms/ACTOR-CRITIC/agent.py
@@ -0,0 +1,176 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+
+def discount_rewards(r, gamma):
+    discounted_r = torch.zeros_like(r)
+    running_add = 0
+    for t in reversed(range(0, r.size(-1))):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+
+class Policy(torch.nn.Module):
+    def __init__(self, state_space, action_space):
+        super().__init__()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden = 64
+        self.tanh = torch.nn.Tanh()
+
+        """
+            Actor network
+        """
+        self.fc1_actor = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_actor = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_actor_mean = torch.nn.Linear(self.hidden, action_space)
+        
+        # Learned standard deviation for exploration at training time 
+        self.sigma_activation = F.softplus
+        init_sigma = 0.5
+        self.sigma = torch.nn.Parameter(torch.zeros(self.action_space)+init_sigma)
+
+
+        """
+            Critic network
+        """
+        # TASK 3: critic network for actor-critic algorithm
+        self.fc1_critic = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_critic = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_critic = torch.nn.Linear(self.hidden, 1)
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.normal_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+
+    def forward(self, x):
+        """
+            Actor
+        """
+        x_actor = self.tanh(self.fc1_actor(x))
+        x_actor = self.tanh(self.fc2_actor(x_actor))
+        action_mean = self.fc3_actor_mean(x_actor)
+
+        sigma = self.sigma_activation(self.sigma)
+        normal_dist = Normal(action_mean, sigma)
+
+        """
+            Critic
+        """
+        x_critic = self.tanh(self.fc1_critic(x))
+        x_critic = self.tanh(self.fc2_critic(x_critic))
+        estimate = self.fc3_critic(x_critic)
+
+        return normal_dist, estimate
+
+class ValueNetwork(torch.nn.Module):
+    def __init__(self, input_dim):
+        super(ValueNetwork, self).__init__()
+        self.fc1 = torch.nn.Linear(input_dim, 128)
+        self.fc2 = torch.nn.Linear(128, 64)
+        self.fc3 = torch.nn.Linear(64, 1)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+class Agent(object):
+    def __init__(self, policy, device='cpu'):
+        self.train_device = device
+        self.policy = policy.to(self.train_device)
+        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
+        self.value_network = None
+
+        self.gamma = 0.99
+        self.states = []
+        self.next_states = []
+        self.action_log_probs = []
+        self.rewards = []
+        self.done = []
+
+
+    def update_policy(self):
+        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)
+        states = torch.stack(self.states, dim=0).to(self.train_device).squeeze(-1)
+        next_states = torch.stack(self.next_states, dim=0).to(self.train_device).squeeze(-1)
+        rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
+        done = torch.Tensor(self.done).to(self.train_device)
+
+        numberOfSteps = len(states)
+        advantages = []
+        current_state_values = []
+
+        total_rewards = []
+        D1 = 0
+        for r in range(len(rewards)-1, -1, -1): 
+            D1 = r + self.gamma * D1
+            total_rewards.insert(0, D1)
+        
+        for timestep in range(len(states)):
+            _, current_value = self.policy(states[timestep]) 
+            _, onestep_value = self.policy(next_states[timestep])
+
+            current_state_values.append(current_value)
+            ad = rewards[timestep] + self.gamma * onestep_value - current_value
+            advantages.append((ad).detach())
+
+        advantages = torch.stack(advantages, dim = 0)
+
+        actor_loss = []
+        for i in range(len(action_log_probs)):
+            actor_loss.append(action_log_probs[i] * advantages[i])
+
+        actor_loss = torch.stack(actor_loss).sum()
+
+        batch_loss = torch.nn.MSELoss()
+
+        total_rewards = torch.tensor(total_rewards)
+        current_state_values = torch.stack(current_state_values, dim = 0).squeeze(-1)
+        
+        critic_loss = batch_loss(current_state_values, total_rewards)
+
+        loss = actor_loss + critic_loss
+        loss = loss * (-1)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.action_log_probs,self.states,self.next_states,self.rewards,self.done = [],[],[],[],[]
+
+      
+
+
+    def get_action(self, state, evaluation=False):
+        """ state -> action (3-d), action_log_densities """
+        x = torch.from_numpy(state).float().to(self.train_device)
+
+        normal_dist = self.policy(x)
+
+        if evaluation:  # Return mean
+            return normal_dist.mean, None
+
+        else:   # Sample from the distribution
+            action = normal_dist.sample()
+
+            # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
+            action_log_prob = normal_dist.log_prob(action).sum()
+
+            return action, action_log_prob
+
+
+    def store_outcome(self, state, next_state, action_log_prob, reward, done):
+        self.states.append(torch.from_numpy(state).float())
+        self.next_states.append(torch.from_numpy(next_state).float())
+        self.action_log_probs.append(action_log_prob)
+        self.rewards.append(torch.Tensor([reward]))
+        self.done.append(done)
+
